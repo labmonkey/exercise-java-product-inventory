8dd424517a0c76867396e3d4368c17af28947311
Pawel Derehajlo <derehajlo@gmail.com>
2025-05-08 18:12:37 +0200

    feat: Initial commit with project structure

---
+++ src/main/java/com/exercise/ExerciseJavaApplication.java
@@ -0,0 +1,13 @@
+package com.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseJavaApplication.class, args);
+	}
+
+}
